518f69b1f73ecd2a5f4a477afb713c38ddaba0db
tejolteon <tiago_jolteon@hotmail.com>
2018-02-22 21:30:43 -0300

    v0.2

--- MangaSelenium/Program.cs
+++ MangaSelenium/Program.cs
@@ -17,23 +17,48 @@ namespace MangaSelenium
     {
         static void Main(string[] args)
         {
-            Inicio();
+            string unionMangas = "http://unionmangas.cc";
+            string manga = "Abara";
+            Inicio(unionMangas,manga);
         }
 
-        public static void Inicio()
+        public static void Inicio(string url, string nomeDoManga)
         {
             IWebDriver driver = new ChromeDriver();
             try
             {
-                driver.Navigate().GoToUrl("http://www.google.com");
+                driver.Navigate().GoToUrl(url);
                 driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-                driver.FindElement(By.XPath("//*[@id=\"gbw\"]/div/div/div[1]/div[2]/a")).Click();
-                driver.FindElement(By.XPath("//*[@id=\"lst-ib\"]")).SendKeys("Google");
-                driver.FindElement(By.XPath("//*[@id=\"lst-ib\"]")).Submit();
-                string att = driver.FindElement(By.XPath("//*[@id=\"rg_s\"]/div[1]/a/img")).GetAttribute("src");
-                driver.Navigate().GoToUrl(att);
+
+                driver.FindElement(By.XPath("//*[@id=\"pesquisa\"]")).SendKeys(nomeDoManga);
+                driver.FindElement(By.XPath("//*[@id=\"pesquisa\"]")).Submit();
                 driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-                DownloadFile(driver, att, @"C:\Users\dev.tiago\Documents\Projects\MangaSelenium\prints\image2.png");
+
+                driver.FindElement(By.XPath("/html/body/div[1]/div/div[1]/div[6]/div[1]/a[2]")).Click();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+                nomeDoManga = driver.FindElement(By.XPath("/html/body/div[1]/div/div[1]/div[1]/div/h2")).Text;
+                var capitulos = driver.FindElements(By.XPath("//*[@class='row lancamento-linha']/div[1]/a"));
+
+                List<string> capUrl = new List<string>();
+                List<string> capTitle = new List<string>();
+
+                foreach (var item in capitulos)
+                {
+                    capUrl.Add(item.GetAttribute("href"));
+                    capTitle.Add(item.Text);
+                }
+
+
+                for (int i = capUrl.Count - 1; i >= 0; i--)
+                {
+                    string path = Path.Combine(Environment.CurrentDirectory, @"Data\", nomeDoManga+"\\"+capTitle[i]);
+
+                    if (!Directory.Exists(path))
+                        Directory.CreateDirectory(path);
+
+                    OpenCap(driver, capUrl[i], nomeDoManga, path);
+                }       
             }
             catch (Exception e)
             {
@@ -61,6 +86,25 @@ namespace MangaSelenium
             }
         }
 
+        public static void OpenCap(this IWebDriver driver, string capUrl, string nomeDoManga, string path)
+        {
+            driver.Navigate().GoToUrl(capUrl);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
+            var images = driver.FindElements(By.XPath("//*[@id=\"leitor\"]/div[4]/div[4]/img"));
+            List<string> imageUrl = new List<string>();
+
+            foreach (var item in images)
+            {
+                imageUrl.Add(item.GetAttribute("src"));
+
+            }
+            string newPath;
+            for (int i = imageUrl.Count-1; i >= 0; i--)
+            {
+                newPath = path + "\\" + (imageUrl.Count - i) + ".jpg";
+                DownloadFile(driver, imageUrl[i], newPath);
+            }     
+        }
     }
 }
